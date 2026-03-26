fead036af1b41efcb121e8663507f713efafe2d4
Tim Gill <timgillviola@gmail.com>
2026-03-26 20:23:47 +0000

    update

--- api/admin/register.js
+++ api/admin/register.js
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
   const newUser = {
     id: randomUUID(),
     email,
-    passwordHash: await bcrypt.hash(password, 12),
+    passwordHash: await bcrypt.hash(password, 10),
   };
 
   users.push(newUser);
